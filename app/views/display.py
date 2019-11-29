f4c262467e0fca1aec245495c19e81b14374abf4
baptiste <baptiste.name>
2019-11-29 10:53:43 +0100

    affichage fonctionnel sauf n cases sur dernière feuille

--- app/views/display.py
+++ app/views/display.py
@@ -7,7 +7,7 @@ import config.settings as cst
 class Display:
     def __init__(self):
         pygame.init()
-        self.view = pygame.display.set_mode((64*cst.LENGTH, 64*cst.WIDTH))
+        self.view = pygame.display.set_mode((32 * cst.LENGTH, 32 * cst.WIDTH))
 
         self.macgyver = pygame.image.load("resources/MacGyver.png").convert()
         self.enemy = pygame.image.load("resources/enemy.png").convert()
@@ -30,61 +30,67 @@ class Display:
         self.left_right_down = pygame.image.load(
             "resources/left_right_down.jpg"
         ).convert()
-        self.left_up_down = pygame.image.load(
-            "resources/left_up_down.jpg"
-        ).convert()
-        self.left_up_right = pygame.image.load(
-            "resources/left_up_right.jpg"
-        ).convert()
-        self.up_right_down = pygame.image.load(
-            "resources/up_right_down.jpg"
-        ).convert()
+        self.left_up_down = pygame.image.load("resources/left_up_down.jpg").convert()
+        self.left_up_right = pygame.image.load("resources/left_up_right.jpg").convert()
+        self.up_right_down = pygame.image.load("resources/up_right_down.jpg").convert()
 
-    def draw_maze(self, tree: "Tree", hero: "Hero", path_enemy: str, path_object: str, ):
-        i, j = 0, 0
-        for leaf_path in tree.leaves:
-            for index in range(1, len(leaf_path) + 2):
-                current_path = leaf_path[:index]
-                if current_path[-1] == "l":
-                    j += -1
-                elif current_path[-1] == "r":
-                    j += 1
-                elif current_path[-1] == "u":
-                    i += 1
-                elif current_path[-1] == "d":
+    def draw_maze(
+        self, tree: "Tree", hero: "Hero", path_enemy: str, path_object: str,
+    ):
+        # print the first cell
+        if tree.root.right and not tree.root.down:
+            self.view.blit(self.right, (0, 0))
+        elif not tree.root.right and tree.root.down:
+            self.view.blit(self.down, (0, 0))
+        elif tree.root.right and tree.root.down:
+            self.view.blit(self.right_down, (0, 0))
+
+        for path in tree.leaves:
+            current_path = ""
+            i, j = 0, 0
+            for direction in path:
+                current_path += direction
+
+                if direction == "l":
                     i += -1
+                elif direction == "r":
+                    i += 1
+                elif direction == "u":
+                    j += -1
+                elif direction == "d":
+                    j += 1
 
-                current = tree.huffman_traversal(current_path)
+                current_node = tree.huffman_traversal(current_path)
 
-                if current.left and current.up and current.right:
-                    self.view.blit(self.left_up_right, (64*i, 64*j))
-                elif current.left and current.up and current.down:
-                    self.view.blit(self.left_up_down, (64*i, 64*j))
-                elif current.left and current.right and current.down:
-                    self.view.blit(self.left_right_down, (64*i, 64*j))
-                elif current.up and current.right and current.down:
-                    self.view.blit(self.up_right_down, (64*i, 64*j))
+                if current_node.left and current_node.up and current_node.right:
+                    self.view.blit(self.left_up_right, (32 * i, 32 * j))
+                elif current_node.left and current_node.up and current_node.down:
+                    self.view.blit(self.left_up_down, (32 * i, 32 * j))
+                elif current_node.left and current_node.right and current_node.down:
+                    self.view.blit(self.left_right_down, (32 * i, 32 * j))
+                elif current_node.up and current_node.right and current_node.down:
+                    self.view.blit(self.up_right_down, (32 * i, 32 * j))
 
-                elif current.left and current.up:
-                    self.view.blit(self.left_up, (64*i, 64*j))
-                elif current.left and current.right:
-                   self.view.blit( self.left_right, (64*i, 64*j))
-                elif current.left and current.down:
-                    self.view.blit(self.left_down, (64*i, 64*j))
-                elif current.up and current.right:
-                    self.view.blit(self.up_right, (64*i, 64*j))
-                elif current.up and current.down:
-                    self.view.blit(self.up_down, (64*i, 64*j))
-                elif current.right and current.down:
-                    self.view.blit(self.right_down, (64*i, 64*j))
+                elif current_node.left and current_node.up:
+                    self.view.blit(self.left_up, (32 * i, 32 * j))
+                elif current_node.left and current_node.right:
+                    self.view.blit(self.left_right, (32 * i, 32 * j))
+                elif current_node.left and current_node.down:
+                    self.view.blit(self.left_down, (32 * i, 32 * j))
+                elif current_node.up and current_node.right:
+                    self.view.blit(self.up_right, (32 * i, 32 * j))
+                elif current_node.up and current_node.down:
+                    self.view.blit(self.up_down, (32 * i, 32 * j))
+                elif current_node.right and current_node.down:
+                    self.view.blit(self.right_down, (32 * i, 32 * j))
 
-                elif current.left:
-                    self.view.blit(self.left, (64*i, 64*j))
-                elif current.right:
-                    self.view.blit(self.right, (64*i, 64*j))
-                elif current.up:
-                    self.view.blit(self.up, (64*i, 64*j))
-                elif current.down:
-                    self.view.blit(self.down, (64*i, 64*j))
-                
+                elif current_node.left:
+                    self.view.blit(self.left, (32 * i, 32 * j))
+                elif current_node.right:
+                    self.view.blit(self.right, (32 * i, 32 * j))
+                elif current_node.up:
+                    self.view.blit(self.up, (32 * i, 32 * j))
+                elif current_node.down:
+                    self.view.blit(self.down, (32 * i, 32 * j))
+
         pygame.display.flip()
